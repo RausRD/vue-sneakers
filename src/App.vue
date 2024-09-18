cb22806da9f73bd0e3d814b8fec602dab8f94fb3
RausRD <dunecruslan2@gmail.com>
2024-09-18 10:34:11 +0300

    add sneakers section

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,19 @@
 <script setup>
+import Card from './components/Card.vue'
 import Header from './components/Header.vue'
 </script>
 
 <template>
-  <div class="bg-white w-4/5 m-auto h-screen rounded-xl shadow-xl mt-14">
+  <div class="bg-white w-4/5 m-auto rounded-xl shadow-xl mt-14">
     <Header />
-    <div>
-      <div class="relative m-10 border border-slate-100 rounded-3xl p-8 cursor-pointer hover:-translate-y-2">
-        <img src="/like-1.svg" alt="Like 1" class="absolute top-8 left-8" />
-        <img src="/sneakers/sneakers-1.jpg" alt="Sneakers" />
-        <p>Мужские Кроссовки Nike Blazer Mid Suede</p>
-        <div class="flex justify-between">
-          <div class="flex flex-col">
-            <span class="text-slate-400">Цена:</span>
-            <b>12 999руб</b>
-          </div>
-          <img src="/plus.svg" alt="Plus" />
-        </div>
+
+    <div class="p-10">
+      <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
+      <div class="grid grid-cols-4 gap-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   </div>
